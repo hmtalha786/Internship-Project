551653a6c111877a1d461d59a5c86946928f140c
Muhammad Talha <talhaofficialwork@gmail.com>
2021-08-04 20:27:21 +0500

    Slider Designed

--- src/components/slides.js
+++ src/components/slides.js
@@ -1,31 +1,81 @@
 import React from "react";
 import dev from "../images/dev.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Carousel } from "react-bootstrap";
 
 const Slides = () => {
   return (
     <div>
-      <br/><br/><br/>
-      <Container>
-        <Row className="justify-content-md-center">
-          <Col xs={12} md={6} className="my-3">
-            <p>Say hello to the pakistan</p>
-            <h3>We provide solutions for your bussinues</h3>
-            <p>
-              Adele stopped singing due to damage to her vocal cords, which
-              prevented her from singing on the doctor's advice to avoid further
-              damage. Adele announced she left music on Twitter, explaining the
-              reason we mentioned, but Adele plans to return gradually.
-            </p>
-            <Button variant="outline-secondary">Stay Connected</Button>
-          </Col>
-          <Col xs={12} md={6} className="my-3">
-            <img src={dev} className="" alt="logo" />
-          </Col>
-        </Row>
-      </Container>
-      <br/><br/><br/>
+      <br />
+      <br />
+      <br />
+      <Carousel>
+        <Carousel.Item>
+          <Container>
+            <Row className="justify-content-md-center">
+              <Col xs={12} md={6} className="mb-3">
+                <h6>Software & the industry redefined</h6><br/>
+                <h1>PROVIDING SOLUTIONS FOR FUTURE BUSSINESS</h1>
+                <p>
+                  We're Engineers driven by a purpose. Our mission is to use
+                  technology to serve humanity to achieve its full potential. We
+                  are experienced in turning entrepreneurial dreams into
+                  reality.
+                </p>
+                {/* <Button variant="outline-secondary">Stay Connected</Button> */}
+              </Col>
+              <Col xs={12} md={6} className="my-3">
+                <img src={dev} className="" alt="logo" />
+              </Col>
+            </Row>
+          </Container>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Container>
+            <Row className="justify-content-md-center">
+              <Col xs={12} md={6} className="my-3">
+                <h6>Software & the industry redefined</h6><br/>
+                <h1>DEVELOPING SOLUTIONS THROUGH INNOVATION</h1>
+                <p>
+                  We simply cannot attempt to be innovative while holding on to
+                  the same mindset as we had before, therefore Techwards is
+                  evolving holistically to engage new technological challenges
+                  head on.
+                </p>
+                {/* <Button variant="outline-secondary">Stay Connected</Button> */}
+              </Col>
+              <Col xs={12} md={6} className="my-3">
+                <img src={dev} className="" alt="logo" />
+              </Col>
+            </Row>
+          </Container>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Container>
+            <Row className="justify-content-md-center">
+              <Col xs={12} md={6} className="my-3">
+                <h6>Software & the industry redefined</h6><br/>
+                <h1>ADDING VALUE TO THE SOCIETY</h1>
+                <p>
+                  As it is said "Data is the new oil” , just like oil...raw data
+                  isn’t valuable in and of itself, but, rather, the value is
+                  created when it is gathered completely and accurately,
+                  allowing you to react to market forces and be proactive and
+                  intentional in your decision-making for progression of the
+                  society.
+                </p>
+                {/* <Button variant="outline-secondary">Stay Connected</Button> */}
+              </Col>
+              <Col xs={12} md={6} className="my-3">
+                <img src={dev} className="" alt="logo" />
+              </Col>
+            </Row>
+          </Container>
+        </Carousel.Item>
+      </Carousel>
+      <br />
+      <br />
+      <br />
     </div>
   );
 };
