64cbc29a5a47eb7a93175e05cdcae7a623821017
Muhammad Talha <talhaofficialwork@gmail.com>
2021-08-04 23:11:23 +0500

    Slider Design Finalized

--- src/components/slides.js
+++ src/components/slides.js
@@ -1,31 +1,35 @@
 import React from "react";
-import dev from "../images/dev.svg";
+import business from "../images/business.svg";
+import value from "../images/value.svg";
+import solutions from "../images/solutions.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col, Carousel } from "react-bootstrap";
 
 const Slides = () => {
   return (
-    <div>
+    <div className="">
       <br />
       <br />
-      <br />
-      <Carousel>
+      <Carousel variant="dark">
         <Carousel.Item>
           <Container>
             <Row className="justify-content-md-center">
-              <Col xs={12} md={6} className="mb-3">
-                <h6>Software & the industry redefined</h6><br/>
-                <h1>PROVIDING SOLUTIONS FOR FUTURE BUSSINESS</h1>
+              <Col xs={12} md={5}>
+                <br />
+                <br />
+                <h6>Software & the industry redefined</h6>
+                <br />
+                <h3>PROVIDING SOLUTIONS FOR FUTURE BUSSINESS</h3>
+                <br />
                 <p>
                   We're Engineers driven by a purpose. Our mission is to use
                   technology to serve humanity to achieve its full potential. We
                   are experienced in turning entrepreneurial dreams into
                   reality.
                 </p>
-                {/* <Button variant="outline-secondary">Stay Connected</Button> */}
               </Col>
-              <Col xs={12} md={6} className="my-3">
-                <img src={dev} className="" alt="logo" />
+              <Col xs={12} md={5}>
+                <img src={business} className="" alt="logo" />
               </Col>
             </Row>
           </Container>
@@ -33,19 +37,22 @@ const Slides = () => {
         <Carousel.Item>
           <Container>
             <Row className="justify-content-md-center">
-              <Col xs={12} md={6} className="my-3">
-                <h6>Software & the industry redefined</h6><br/>
-                <h1>DEVELOPING SOLUTIONS THROUGH INNOVATION</h1>
+              <Col xs={12} md={5}>
+                <br />
+                <br />
+                <h6>Software & the industry redefined</h6>
+                <br />
+                <h3>DEVELOPING SOLUTIONS THROUGH INNOVATION</h3>
+                <br />
                 <p>
                   We simply cannot attempt to be innovative while holding on to
                   the same mindset as we had before, therefore Techwards is
                   evolving holistically to engage new technological challenges
                   head on.
                 </p>
-                {/* <Button variant="outline-secondary">Stay Connected</Button> */}
               </Col>
-              <Col xs={12} md={6} className="my-3">
-                <img src={dev} className="" alt="logo" />
+              <Col xs={12} md={5}>
+                <img src={solutions} className="" alt="logo" />
               </Col>
             </Row>
           </Container>
@@ -53,9 +60,13 @@ const Slides = () => {
         <Carousel.Item>
           <Container>
             <Row className="justify-content-md-center">
-              <Col xs={12} md={6} className="my-3">
-                <h6>Software & the industry redefined</h6><br/>
-                <h1>ADDING VALUE TO THE SOCIETY</h1>
+              <Col xs={12} md={5}>
+                <br />
+                <br />
+                <h6>Software & the industry redefined</h6>
+                <br />
+                <h3>ADDING VALUE TO THE SOCIETY AND LIFE</h3>
+                <br />
                 <p>
                   As it is said "Data is the new oil” , just like oil...raw data
                   isn’t valuable in and of itself, but, rather, the value is
@@ -64,10 +75,9 @@ const Slides = () => {
                   intentional in your decision-making for progression of the
                   society.
                 </p>
-                {/* <Button variant="outline-secondary">Stay Connected</Button> */}
               </Col>
-              <Col xs={12} md={6} className="my-3">
-                <img src={dev} className="" alt="logo" />
+              <Col xs={12} md={5}>
+                <img src={value} className="" alt="logo" />
               </Col>
             </Row>
           </Container>
