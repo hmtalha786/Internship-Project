2b9bd415ca6e6c0434c144a1ae4937503d48aa24
Muhammad Talha <talhaofficialwork@gmail.com>
2021-08-16 16:43:11 +0500

    Update why.js

--- src/components/sub_components/why.js
+++ src/components/sub_components/why.js
@@ -12,7 +12,7 @@ import Laravel from "../images/Laravel.png";
 import Bootstrap from "../images/Bootstrap.png";
 import Flutter from "../images/Flutter.png";
 import Python from "../images/Python.png";
-import { Col, Container, Row, Button } from "react-bootstrap";
+import { Col, Container, Row, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Why = () => {
@@ -65,109 +65,226 @@ const Why = () => {
 
         <Row className="justify-content-md-center">
           <Col xs={12} md={3} className="text-center my-5">
-            <img
-              src={ReactJs}
-              className="mt-1"
-              style={{ width: "35%" }}
-              alt="React"
-            />
-
-            <h6 className="my-3">React. Js</h6>
+            <Card
+              style={{ width: "16rem" }}
+              className="mx-auto"
+              border="danger"
+            >
+              <Card.Img
+                variant="top"
+                src={ReactJs}
+                className="mx-auto mt-5"
+                style={{ width: "35%" }}
+                alt="React"
+              />
+              <Card.Body>
+                <Card.Text>
+                  <h6 className="my-3">React. Js</h6>
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </Col>
 
           <Col xs={12} md={3} className="text-center my-5">
-            <img src={Angular} style={{ width: "30%" }} alt="Angular" />
-
-            <h6 className="my-3">Angular Js</h6>
+            <Card
+              style={{ width: "16rem" }}
+              className="mx-auto"
+              border="danger"
+            >
+              <Card.Img
+                variant="top"
+                src={Angular}
+                className="mx-auto mt-5"
+                style={{ width: "30%" }}
+                alt="Angular"
+              />
+              <Card.Body>
+                <Card.Text>
+                  <h6 className="my-3">Angular Js</h6>
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </Col>
 
           <Col xs={12} md={3} className="text-center my-5">
-            <img
-              src={Vue}
-              className="my-2"
-              style={{ width: "30%" }}
-              alt="Vue"
-            />
-
-            <h6 className="my-3">Vue.Js</h6>
+            <Card
+              style={{ width: "16rem" }}
+              className="mx-auto"
+              border="danger"
+            >
+              <Card.Img
+                variant="top"
+                src={Vue}
+                className="mx-auto mt-5"
+                style={{ width: "33%" }}
+                alt="Vue"
+              />
+              <Card.Body>
+                <Card.Text>
+                  <h6 className="mt-4 mb-3">Vue. Js</h6>
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </Col>
 
           <Col xs={12} md={3} className="text-center my-5">
-            <img src={Flutter} style={{ width: "30%" }} alt="Flutter" />
-
-            <h6 className="my-3">Flutter</h6>
+            <Card
+              style={{ width: "16rem" }}
+              className="mx-auto"
+              border="danger"
+            >
+              <Card.Img
+                variant="top"
+                src={Flutter}
+                className="mx-auto mt-5"
+                style={{ width: "32%" }}
+                alt="Flutter"
+              />
+              <Card.Body>
+                <Card.Text>
+                  <h6 className="my-3">Flutter</h6>
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
 
         <Row className="justify-content-md-center">
           <Col xs={12} md={3} className="text-center my-5">
-            <img
-              src={Node}
-              className="mt-2 mb-1"
-              style={{ width: "36%" }}
-              alt="Node"
-            />
-            <h6 className="my-3">Node. Js</h6>
+            <Card
+              style={{ width: "16rem" }}
+              className="mx-auto"
+              border="danger"
+            >
+              <Card.Img
+                variant="top"
+                src={Node}
+                className=" mx-auto mt-5"
+                style={{ width: "54%" }}
+                alt="Node"
+              />
+              <Card.Body>
+                <Card.Text>
+                  <h6 className="my-3">Node. Js</h6>
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </Col>
 
           <Col xs={12} md={3} className="text-center my-5">
-            <img src={Laravel} style={{ width: "25%" }} alt="Laravel" />
-            <h6 className="my-3">PHP / Laravel</h6>
+            <Card
+              style={{ width: "16rem" }}
+              className="mx-auto"
+              border="danger"
+            >
+              <Card.Img
+                variant="top"
+                className=" mx-auto mt-4 mb-2"
+                src={Laravel}
+                style={{ width: "44%" }}
+                alt="Laravel"
+              />
+              <Card.Body>
+                <Card.Text>
+                  <h6 className="mb-3">PHP / Laravel</h6>
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </Col>
-
           <Col xs={12} md={3} className="text-center my-5">
-            <img src={Net} style={{ width: "25%" }} alt=".Net" />
-            <h6 className="my-3">.Net & .NET Core</h6>
+            <Card
+              style={{ width: "16rem" }}
+              className="mx-auto"
+              border="danger"
+            >
+              <Card.Img
+                variant="top"
+                className=" mx-auto mt-4"
+                src={Net}
+                style={{ width: "49%" }}
+                alt=".Net"
+              />
+              <Card.Body>
+                <Card.Text>
+                  <h6 className="mb-3">.Net & .NET Core</h6>
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
 
         <Row className="justify-content-md-center">
           <Col xs={12} md={3} className="text-center my-5">
-            <img
-              src={Bootstrap}
-              className="mt-2"
-              style={{ width: "30%" }}
-              alt="Bootstrap"
-            />
-            <h6 className="my-3">HTML / Bootstrap</h6>
+            <Card
+              style={{ width: "16rem" }}
+              className="mx-auto"
+              border="danger"
+            >
+              <Card.Img
+                variant="top"
+                className=" mx-auto mt-5"
+                src={Bootstrap}
+                style={{ width: "45%" }}
+                alt="Bootstrap"
+              />
+              <Card.Body>
+                <Card.Text>
+                  <h6 className="my-3">Bootstrap</h6>
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </Col>
-
           <Col xs={12} md={3} className="text-center my-5">
-            <img src={Python} style={{ width: "26%" }} alt="Python" />
-            <h6 className="my-3">Python</h6>
+            <Card
+              style={{ width: "16rem" }}
+              className="mx-auto"
+              border="danger"
+            >
+              <Card.Img
+                variant="top"
+                className=" mx-auto mt-5"
+                src={Python}
+                style={{ width: "40%" }}
+                alt="Python"
+              />
+              <Card.Body>
+                <Card.Text>
+                  <h6 className="mt-1 mb-3">Python</h6>
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
 
         <Row className="justify-content-md-center mx-auto text-center">
-          <Link to="/services">
-            <Button variant="outline-danger" type="submit">
-              <strong>See More</strong>
-            </Button>
-          </Link>
+          <Col md={3} className="mx-auto">
+            <Link to="/services">
+              <Button variant="outline-danger" type="submit">
+                <strong>See More</strong>
+              </Button>
+            </Link>
+          </Col>
         </Row>
 
         <br />
 
         <Row className="justify-content-md-center my-5">
-          <Col xs={12} md={4} className="mx-auto text-center my-3">
+          <Col xs={12} md={5} className="text-center mx-auto my-3">
             <img src={mission} alt="logo" />
-          </Col>
-          <Col xs={12} md={6} className="my-3">
+            <br />
+            <br />
             <h3 className="text">Our Mission</h3>
             <p>
               Our mission is to empower visionaries to transform their ideas
               into reality. We Build innovative Aapplications for Startups and
-              Corporations to fulfill their business needs. Look no further, We
-              are here to guide and find success for you in the digital world.
+              Corporations to fulfill their business needs. We are here to guide
+              and find success for you in the digital world.
             </p>
           </Col>
-        </Row>
-
-        <Row className="justify-content-md-center my-5">
-          <Col xs={12} md={4} className="mx-auto text-center my-3">
+          <Col xs={12} md={5} className="text-center mx-auto my-3">
             <img src={idealogy} alt="logo" />
-          </Col>
-          <Col xs={12} md={6} className="my-3">
+            <br />
+            <br />
             <h3 className="text">Our Idealogy</h3>
             <p>
               We are driven by our values & principles. At ScientificBit, we
